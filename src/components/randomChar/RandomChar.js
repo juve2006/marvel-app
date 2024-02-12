0a443e8d60b117edd8f01e2ba98b00eb186ca839
juve2006 <arnika200@gmail.com>
2024-02-12 15:42:59 +0200

    add spinner

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -1,68 +1,82 @@
 import { Component } from 'react';
 
+import Spinner from '../spinner/Spinner';
+
 import './randomChar.scss';
 import mjolnir from '../../resources/img/mjolnir.png';
 import MarvelService from '../../services/MarvelService';
 
 export default class RandomChar extends Component {
     constructor(props) {
-      super(props);
-      this.updateCharacter();
+        super(props);
+        this.updateCharacter();
     }
 
     state = {
-      char: {}
+        char: {},
+        loading: true
     }
 
     marvelService = new MarvelService();
 
     onCharLoaded = (char) => {
-      this.setState({ char })
+        this.setState({
+            char,
+            loading: false
+        })
     }
 
     updateCharacter = () => {
-      const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);
-      this.marvelService
-          .getCharacter(id)
-          .then(this.onCharLoaded)
+        const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);
+        this.marvelService
+            .getCharacter(id)
+            .then(this.onCharLoaded)
     }
 
     render() {
-      const {char : { name, description, thumbnail, homepage, wiki }} = this.state;
+        const {char, loading} = this.state;
 
-      return (
-        <div className="randomchar">
-          <div className="randomchar__block">
+        return (
+            <div className="randomchar">
+                {loading ? <Spinner/> : <View char={char}/>}
+                <div className="randomchar__static">
+                    <p className="randomchar__title">
+                        Random character for today!<br/>
+                        Do you want to get to know him better?
+                    </p>
+                    <p className="randomchar__title">
+                        Or choose another one
+                    </p>
+                    <button className="button button__main">
+                        <div className="inner">try it</div>
+                    </button>
+                    <img src={mjolnir} alt="mjolnir" className="randomchar__decoration"/>
+                </div>
+            </div>
+        );
+    }
+}
+
+function View ({char}) {
+    const { name, thumbnail, description, homepage, wiki } = char;
+
+    return (
+        <div className="randomchar__block">
             <img src={thumbnail} alt="Random character" className="randomchar__img"/>
             <div className="randomchar__info">
-              <p className="randomchar__name">{name}</p>
-              <p className="randomchar__descr">
-                {description}
-              </p>
-              <div className="randomchar__btns">
-                <a href={homepage} className="button button__main">
-                  <div className="inner">homepage</div>
-                </a>
-                <a href={wiki} className="button button__secondary">
-                  <div className="inner">Wiki</div>
-                </a>
-              </div>
+                <p className="randomchar__name">{name}</p>
+                <p className="randomchar__descr">
+                    {description}
+                </p>
+                <div className="randomchar__btns">
+                    <a href={homepage} className="button button__main">
+                        <div className="inner">homepage</div>
+                    </a>
+                    <a href={wiki} className="button button__secondary">
+                        <div className="inner">Wiki</div>
+                    </a>
+                </div>
             </div>
-          </div>
-          <div className="randomchar__static">
-            <p className="randomchar__title">
-              Random character for today!<br/>
-              Do you want to get to know him better?
-            </p>
-            <p className="randomchar__title">
-              Or choose another one
-            </p>
-            <button className="button button__main">
-              <div className="inner">try it</div>
-            </button>
-            <img src={mjolnir} alt="mjolnir" className="randomchar__decoration"/>
-          </div>
         </div>
-      )
-    }
+    );
 }
