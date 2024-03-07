1cee7eb3910472e2dfd5ff35c0a678944d9d310b
juve2006 <arnika200@gmail.com>
2024-03-07 10:11:36 +0200

    add routing

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,35 +1,24 @@
-import {useState} from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
+import {MainPage, ComicsPage} from '../pages';
 import AppHeader from '../appHeader/AppHeader';
-import RandomChar from '../randomChar/RandomChar';
-import CharList from '../charList/CharList';
-import CharInfo from '../charInfo/CharInfo';
-import ComicsList from '../comicsList/ComicsList';
-import ErrorBoundary from '../errorBoundary/ErrorBoundary';
-
-import decoration from '../../resources/img/vision.png';
 
 export default function App() {
-    const [selectedChar, setSelectedChar] = useState(null);
-
-    function onCharSelected(id) {
-        setSelectedChar(id);
-    }
-
-    return (
+  return (
+      <Router>
         <div className="app">
-            <AppHeader/>
-            <main>
-                {/*<ComicsList/>*/}
-                <RandomChar/>
-                <div className="char__content">
-                    <CharList onCharSelected={onCharSelected}/>
-                    <ErrorBoundary>
-                        <CharInfo charId={selectedChar}/>
-                    </ErrorBoundary>
-                </div>
-                <img className="bg-decoration" src={decoration} alt="vision"/>
-            </main>
+          <AppHeader/>
+          <main>
+            <Switch>
+              <Route exact path="/">
+                <MainPage/>
+              </Route>
+              <Route exact path="/comics">
+                <ComicsPage/>
+              </Route>
+            </Switch>
+          </main>
         </div>
-    );
+      </Router>
+  );
 }
---
+++ src/components/pages/ComicsPage.js
@@ -0,0 +1,11 @@
+import ComicsList from '../comicsList/ComicsList';
+import AppBanner from '../appBanner/AppBanner';
+
+export default function ComicsPage() {
+  return (
+    <>
+      <AppBanner />
+      <ComicsList />
+    </>
+  );
+}
